be9d6ced15555e9671f3bc3768c0d8aa64a63ad3
wachehi <wachehi@hotmail.com>
2021-02-13 23:39:44 +0100

    deploy.sh : ajout de l'entête

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#################################################################
+# Decription : déploiement à la volée de conteneur
+#
+# Auteur : Ali
+#
+# Date : 28/12/2021
+#
+##################################################################
+
+echo "
+     
+Options :
+     - --create : lancer des conteneurs
+        
+     - --drop :   supprimer les conteneurs créer par le deploy.sh 
+
+     - --infos :  caractéristiques des conteneurs (ip, nom , user ...)
+
+      - --start :  redémarrage des conteneurs
+
+      - --ansible : déploiement arborescence ansible
+
+"
