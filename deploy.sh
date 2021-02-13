585dd6714bd0ed62fb7132dcb9f074a3eb39fbe4
wachehi <wachehi@hotmail.com>
2021-02-14 00:24:14 +0100

    [DEV] : mise en place des conditions pour les options

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,44 @@
 # Date : 28/12/2021
 #
 ##################################################################
+#si option --create
+if [ "$1" == "--create" ];then
+
+   echo " " 
+   echo "notre option est --create"
+   echo " "
+
+#si option --drop
+elif [ "$1" == "--drop" ];then
+  
+   echo " " 
+   echo "notre option est --drop"
+   echo " "
+
+#si option --start
+elif [ "$1" == "--start" ];then
+  
+   echo " " 
+   echo "notre option est --start"
+   echo " "
+
+#si option --ansible
+elif [ "$1" == "--ansible" ];then
+  
+   echo " " 
+   echo "notre option est --ansible"
+   echo " "
+
+#si option --infos
+elif [ "$1" == "--infos" ];then
+  
+   echo " " 
+   echo "notre option est --infos"
+   echo " "
+
+
+# si aucune option affichage de l'aide
+else
 
 echo "
      
@@ -18,8 +56,10 @@ Options :
 
      - --infos :  caractéristiques des conteneurs (ip, nom , user ...)
 
-      - --start :  redémarrage des conteneurs
+     - --start :  redémarrage des conteneurs
 
-      - --ansible : déploiement arborescence ansible
+     - --ansible : déploiement arborescence ansible
 
 "
+fi
+
